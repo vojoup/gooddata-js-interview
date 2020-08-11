c2c420c9f530d828f14ea374d1bebd7f645d66e1
vojoup <voj.oup@gmail.com>
2020-08-11 11:20:44 +0200

    Add constants

---
+++ src/config/config.js
@@ -0,0 +1,5 @@
+// Some of these could be env variables if they are configured globally
+export const GROSS_PROFIT_MEASURE = '/gdc/md/xms7ga4tf3g3nzucd8380o2bev8oeknp/obj/6877';
+export const DATE_ATTRIBUTE_IN_MONTHS = '/gdc/md/xms7ga4tf3g3nzucd8380o2bev8oeknp/obj/2142';
+export const DATE_ATTRIBUTE = '/gdc/md/xms7ga4tf3g3nzucd8380o2bev8oeknp/obj/2180';
+export const PROJECT_ID = 'xms7ga4tf3g3nzucd8380o2bev8oeknp'
